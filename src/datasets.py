afc922afc5ddd5d60845c268562184319465f9d4
yassakamoto <yassakamoto@gmail.com>
2024-07-17 05:50:36 +0000

    Added new file tough it is 1.4point

--- src/datasets.py
+++ src/datasets.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 from typing import Tuple
 from termcolor import cprint
-
+from sklearn.preprocessing import StandardScaler    ##追加
 
 class ThingsMEGDataset(torch.utils.data.Dataset):
     def __init__(self, split: str, data_dir: str = "data") -> None:
@@ -19,6 +19,17 @@ class ThingsMEGDataset(torch.utils.data.Dataset):
         if split in ["train", "val"]:
             self.y = torch.load(os.path.join(data_dir, f"{split}_y.pt"))
             assert len(torch.unique(self.y)) == self.num_classes, "Number of classes do not match."
+
+        """
+        #追加：スケーリング(標準化)  平均0、分散1に各時系列データ（self.X の各チャネルごと）に処理
+        # トレーニングデータでStandardScalerをfitし、同じものをvalid、testでも利用。時系列データの最後の次元に沿ってスケーリングを行う
+        self.scaler = StandardScaler()
+        if split == "train":
+            self.X = self.scaler.fit_transform(self.X.reshape(-1, self.X.shape[-1])).reshape(self.X.shape)
+        else:
+            self.X = self.scaler.transform(self.X.reshape(-1, self.X.shape[-1])).reshape(self.X.shape)
+        """
+
 
     def __len__(self) -> int:
         return len(self.X)
